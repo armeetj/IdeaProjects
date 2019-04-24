adde40bd037f09f4f83f50f622d7584afd933850
Armeet Jatyani <xarmeetx@gmail.com>
2019-04-23 23:37:39 -0700

    -

--- Codeforces/src/CodeforcesBetaRound1/TheatreSquare.java
+++ Codeforces/src/CodeforcesBetaRound1/TheatreSquare.java
@@ -1,9 +1,29 @@
 package CodeforcesBetaRound1;
 
+import java.util.Scanner;
+
 public class TheatreSquare
 {
     public static void main(String[] args)
     {
-        test
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextInt();
+        long m = scanner.nextInt();
+        long a = scanner.nextInt();
+
+        if (n % a ==0 && m % a == 0)
+        {
+            System.out.println((n * m) / (a * a));
+        }else
+        {
+            if(n%a == 0 && m%a != 0)
+            {
+
+            }
+        }
+
+
+
     }
 }
