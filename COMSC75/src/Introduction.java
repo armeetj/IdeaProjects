5bc6991309c2a9a5ac4e8b828b8ec3b970d5e95d
xarmeetx <xarmeetx@gmail.com>
2019-06-17 11:50:47 -0700

    finished Introduction.java

--- COMSC75/src/Introduction.java
+++ COMSC75/src/Introduction.java
@@ -1,19 +1,26 @@
+//Armeet Singh Jatyani
+//Introduction
+/*
+Write a Java program that prints my name, favorite short poem, author, age in years, age in days, and the product
+of my favorite number, 123456789, and 9
+
+We also learn how to make new lines using the \n character
+ */
+
 public class Introduction
 {
+    //make a static constant using the final keyword with type integer, all methods in the class can access this variable
+    static final int DAYS_IN_YEAR = 365;
+
     public static void main(String[] args)
     {
-        //use the System.out.println(); statement to print something out to standard output
+        //use the print statement to print something out to standard output
         //the \n at the end creates a new line, or a space after what we are printing
-        //alternatively we could type in System.out.println(); after this line of code to make a new line after writing our name
         System.out.println("My name is Armeet Jatyani.\n");
 
-
-        //this is one of my favorite poems, by Robert Frost, called "The Road Not Taken"
         /*
-        Instead of using one print statement and using the \n new line character,
-        I chose to use multiple print statements right after each other to clean up the code
-        (Instead of having one long line of code that stretches off the screen, I prefer multiple lines so all code can
-        be seen
+        I used multiple print statements because instead of having one long line of code that stretches off the screen,
+        I prefer multiple lines so all code can be seen
          */
         System.out.println("I shall be telling this with a sigh");
         System.out.println("Somewhere ages and ages hence:");
@@ -22,27 +29,26 @@ public class Introduction
         System.out.println("And that has made all the difference.");
         System.out.println("    --Robert Frost");
 
-        //make a new line, using the System.out.println(); command to just print/skip a line
+        //make a new line, using the print command to just print a blank line
         System.out.println();
 
-        //here I store my age in a local variable called age of type integer
-        int age = 14;
-        //print out the age that is stored in the local variable
-        System.out.println(age);
+        //here I store my age in a local integer variable called ageInYears
+        int ageInYears = 14;
+        //print out the age in years
+        System.out.println(ageInYears);
+
         //print out the result of the yearsToDays function, passing in the age in years as the argument
-        System.out.println(yearsToDays(age));
+        System.out.println(yearsToDays(ageInYears));
 
         //here I store my favorite number in another variable with an integer datatype
         int favNumber = 1;
-        System.out.println(12345679 * 9 *  favNumber);
+        System.out.println(12345679 * 9 * favNumber);
     }
 
     public static int yearsToDays(int years)
     {
-        //this function takes in the age in years of a person
+        //this function takes in the age in years of a person and returns the age in days
         //it is public and static so it can be accessed from the main method
-        //it returns an integer
-        //return years * 365 because there are 365 days per year
-        return (years * 365);
+        return (years * DAYS_IN_YEAR);
     }
 }
