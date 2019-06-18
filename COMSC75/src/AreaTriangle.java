beecc25d4da11ab4f3831ad7b80b71b1471841e2
Armeet Jatyani <38377327+xarmeetx@users.noreply.github.com>
2019-06-18 11:47:47 -0700

    added the header to AreaTriangle.java

--- COMSC75/src/AreaTriangle.java
+++ COMSC75/src/AreaTriangle.java
@@ -1,3 +1,12 @@
+//Armeet Singh Jatyani
+//Variables / AreaTriangle
+/*
+	In this program, we prompt the user for 3 coordinate pairs.
+	We use the distance between two points formulas and Heron's formula
+	to find the area of this triangle.
+*/
+
+//import the necessary libraries
 import java.lang.Math;
 import java.util.Scanner;
 
@@ -11,7 +20,7 @@ public class AreaTriangle
 		//prompt the user to enter 3 coordinate pairs
 		System.out.println("Enter the coordinates of three points" +
 		"seperated by spaces");
-		System.out.println("like x1 y1 x2 y2 x3 y3: 1.5 -3.4 4.6 5 9.5 -3.4 ");
+		System.out.println("like x1 y1 x2 y2 x3 y3: 1.3 -3.03 4.7 8 2.5 -8.4 ");
 
 		//store the user's coordinate pairs as variables
 		double x1 = scanner.nextDouble();
@@ -26,7 +35,9 @@ public class AreaTriangle
 		double side2 = distance(x2, y2, x3, y3);
 		double side3 = distance(x1, y1, x3, y3);
 
+		//calculate the semiperimeter of the traingle
 		double semiPerimeter = (side1 + side2 + side3)/2;
+
 		//calculate area of the rectangle
 		double area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) *
 		(semiPerimeter - side2) * (semiPerimeter - side3));
