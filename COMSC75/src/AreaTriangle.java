795432c786b2dd58f1f78fe8faa99fc56ed19b6f
Armeet Jatyani <38377327+xarmeetx@users.noreply.github.com>
2019-06-19 11:59:18 -0700

    Update AreaTriangle.java

--- COMSC75/src/AreaTriangle.java
+++ COMSC75/src/AreaTriangle.java
@@ -22,7 +22,8 @@ public class AreaTriangle
 		//prompt the user to enter 3 coordinate pairs
 		System.out.println("Enter the coordinates of three points" +
 		"seperated by spaces");
-		System.out.println("like x1 y1 x2 y2 x3 y3: 1.3 -3.03 4.7 8 2.5 -8.4 ");
+		System.out.println("like x1 y1 x2 y2 x3 y3: 1.3 -3.03 4.7 8" +
+		"2.5 -8.4 ");
 
 		//store the user's coordinate pairs as variables
 		double x1 = scanner.nextDouble();
@@ -45,7 +46,7 @@ public class AreaTriangle
 		(semiPerimeter - side2) * (semiPerimeter - side3));
 
 		//print out the area to the user
-		System.out.println("The area of the triangle is " + area);
+		System.out.printf("The area of the triangle is %.1f ", area);
 
 	}
 
