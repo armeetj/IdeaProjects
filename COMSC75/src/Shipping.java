46cd2f88fb5c1e63ef4af9c21819f5839cf51f04
xarmeetx <xarmeetx@gmail.com>
2019-06-20 11:50:34 -0700

    added changes 6-20-19

--- COMSC75/src/Shipping.java
+++ COMSC75/src/Shipping.java
@@ -21,6 +21,8 @@ public class Shipping
     System.out.print("Enter the weight of the package in pounds: ");
     //store the weight entered by the user
     double userWeight = scanner.nextDouble();
+
+    //find the weight group and print the price
     if (userWeight > 20)
     {
       System.out.println("The package cannot be shipped.");
